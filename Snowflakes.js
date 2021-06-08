4dc36d39340b6193f1e4f080039031830aa7c531
RUDRAKSHI0 <75970123+RUDRAKSHI0@users.noreply.github.com>
2021-06-08 10:46:39 +0530

    Add files via upload

---
+++ Snowflakes.js
@@ -0,0 +1,32 @@
+class Snowflakes {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            //isStatic:true
+        }
+        this.r = 10;
+        //this.snowflake1=loadImage("snow4.webp");
+       // this.snowflake2=loadImage("snow5.webp");
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.color = color(random(0,255),random(0,255),random(0,255))
+        World.add(world,this.body);
+    }
+display(){
+
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    noStroke();
+    fill(this.color);
+    ellipseMode(RADIUS);
+    ellipse(0,0,this.r,this.r);
+    pop();
+			
+
+}
+}
